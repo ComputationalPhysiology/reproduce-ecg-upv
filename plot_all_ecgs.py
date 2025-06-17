6abe4619be51688ff8ebbde8d0285fceebd4afe9
Henrik Nicolay Finsberg <henriknf@simula.no>
2025-06-17 09:20:48 +0200

    Add some more utility functions

---
+++ plot_all_ecgs.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_ecg(outdir: Path, ax1):
+    if not (outdir / "ecg.csv").exists():
+        print(f"Skipping {outdir.name}: no ecg.csv file")
+        return None, None
+    print(f"Plotting {outdir.name}")
+    df = pd.read_csv(outdir / "ecg.csv")
+
+    fig, ax2 = plt.subplots(2, 3, figsize=(12, 8), sharex=True)
+    for ax in [ax2, ax1]:
+        l, = ax[0, 0].plot(df["time"], df["LA"])
+        ax[0, 0].set_title("LA")
+        ax[0, 1].plot(df["time"], df["RA"])
+        ax[0, 1].set_title("RA")
+        ax[0, 2].plot(df["time"], df["LL"])
+        ax[0, 2].set_title("LL")
+
+        ax[1, 0].plot(df["time"], df["I"])
+        ax[1, 0].set_title("I")
+        ax[1, 1].plot(df["time"], df["II"])
+        ax[1, 1].set_title("II")
+        ax[1, 2].plot(df["time"], df["III"])
+        ax[1, 2].set_title("III")
+
+        for axi in ax.flatten():
+            axi.grid()
+
+    fig.tight_layout()
+    fig.savefig(outdir / "ecg.png")
+    plt.close(fig)
+    return l, df
+
+def main():
+
+    fig, ax = plt.subplots(2, 3, figsize=(15, 8), sharex=True)
+    all_ecg_file = Path("results") / "all_ecg.xlsx"
+    lines = []
+    labels = []
+    with pd.ExcelWriter(all_ecg_file) as writer:
+        for dir in Path("results").iterdir():
+
+            l, df = plot_ecg(dir, ax)
+            if df is None:
+                continue
+
+            df.to_excel(writer, sheet_name=dir.name, index=False)
+            lines.append(l)
+            labels.append(dir.name)
+
+    fig.legend(lines, labels, loc="center right")
+    fig.subplots_adjust(right=0.85)
+    fig.savefig("results/all_ecg.png", dpi=500)
+    plt.close(fig)
+
+if __name__ == "__main__":
+    main()
---
+++ plot_all_single_cell.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import ORdmm
+
+def plot_single_cell(outdir: Path, figdir: Path):
+
+    dt = 1.0
+    BCL = 1000
+    t = np.arange(0, BCL, dt)
+
+    fig, ax = plt.subplots(2, 1, figsize=(12, 8), sharex=True)
+    all_states = []
+    vs = [t]
+    cas = [t]
+    for i, cell_type in enumerate(["endo", "mid", "epi"]):
+        celldir = outdir / "steady-states-0D" / cell_type
+        files = list(celldir.glob("*.npy"))
+
+        if len(files) == 0:
+            print(f"No files found in {celldir}. Skipping.")
+            return None, None, None
+
+        states_file = next(iter(filter(lambda x: x.stem.startswith("steady_states"), files)))
+        tracked_file = next(iter(filter(lambda x: x.stem.startswith("tracked_values"), files)))
+
+        # states = np.load(states_file, allow_pickle=True).item()
+        # tracked = np.load(tracked_file, allow_pickle=True).item()
+        states = np.load(states_file)
+
+        tracked = np.load(tracked_file)
+        all_states.append(dict(zip(ORdmm.state.keys(), states)))
+
+        v = tracked[-len(t):, 0]
+        ca = tracked[-len(t):, 1]
+        vs.append(v)
+        cas.append(ca)
+
+
+        ax[0].plot(t, v, label=cell_type)
+        ax[1].plot(t, ca, label=cell_type)
+
+    ax[0].set_title(outdir.name)
+    ax[0].set_ylabel("Voltage (mV)")
+    ax[1].set_ylabel("Calcium (mM)")
+    ax[1].set_xlabel("Time (ms)")
+    for axi in ax:
+        axi.legend()
+        axi.grid()
+    fig.savefig(figdir / f"{outdir.name}.png")
+    plt.close(fig)
+
+    df = pd.DataFrame(all_states, index=["endo", "mid", "epi"])
+
+    df_v = pd.DataFrame(np.array(vs).T, columns=["time", "endo", "mid", "epi"])
+    df_ca = pd.DataFrame(np.array(cas).T, columns=["time", "endo", "mid", "epi"])
+    return df, df_v, df_ca
+
+def main():
+
+    outdir = Path("results") / "single_cell"
+    outdir.mkdir(parents=True, exist_ok=True) 
+    all_v = outdir / "all_voltage.xlsx"
+    all_ca = outdir / "all_calcium.xlsx"
+    all_states = outdir / "all_initial_states.xlsx"
+    lines = []
+    labels = []
+    with pd.ExcelWriter(all_v) as writer_voltage, \
+         pd.ExcelWriter(all_ca) as writer_calcium, \
+         pd.ExcelWriter(all_states) as writer_states:
+
+        for dir in Path("results").iterdir():
+            print(dir)
+            df, df_v, df_ca = plot_single_cell(dir, outdir)
+            if df is None:
+                continue
+            # Save to Excel files
+            df.to_excel(writer_states, sheet_name=dir.name, index=True)
+            df_v.to_excel(writer_voltage, sheet_name=dir.name, index=False)
+            df_ca.to_excel(writer_calcium, sheet_name=dir.name, index=False)
+
+      
+
+if __name__ == "__main__":
+    main()
